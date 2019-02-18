47b183547d56406d75e9d592d19aae142c79575d
Muntaha Islam <muntaha.islam0019@gmail.com>
2019-02-18 08:18:37 +0600

    Main Files

---
+++ Assets/Scripts/ContinuePlayingMusic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinuePlayingMusic : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Don't destroy the background music when a new
+        // scene loads
+        DontDestroyOnLoad(gameObject);
+    }
+}
